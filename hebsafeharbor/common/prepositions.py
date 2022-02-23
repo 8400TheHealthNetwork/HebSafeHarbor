ff4e69a7b1bed7c356e4d67fa0fb4cfc1c0339b4
Shahar Admati <admatis@microsoft.com>
2022-02-23 16:38:36 +0200

    wip

--- hebsafeharbor/common/prepositions.py
+++ hebsafeharbor/common/prepositions.py
@@ -1,3 +1,4 @@
 LOCATION_PREPOSITIONS = ["מ", "ל", "ב", "ו"]
 DISEASE_PREPOSITIONS = ["מ", "ל", "ב", "ו"]
 MEDICATION_PREPOSITIONS = ["מ", "ל", "ב", "ו"]
+MEDICAL_TEST_PREPOSITIONS = ["מ", "ל", "ב", "ו"]
--- hebsafeharbor/identifier/consolidation/post_consolidation/medical_post_consolidator.py
+++ hebsafeharbor/identifier/consolidation/post_consolidation/medical_post_consolidator.py
@@ -3,7 +3,7 @@ from typing import List, Dict, Set
 from presidio_analyzer import RecognizerResult
 
 from hebsafeharbor import Doc
-from hebsafeharbor.common.prepositions import DISEASE_PREPOSITIONS, MEDICATION_PREPOSITIONS
+from hebsafeharbor.common.prepositions import DISEASE_PREPOSITIONS, MEDICATION_PREPOSITIONS, MEDICAL_TEST_PREPOSITIONS
 from hebsafeharbor.common.terms_recognizer import TermsRecognizer
 from hebsafeharbor.identifier.consolidation.consolidation_config import CATEGORY_TO_CONTEXT_PHRASES, \
     ENTITY_TYPE_TO_CATEGORY
@@ -15,15 +15,18 @@ class MedicalPostConsolidator(PostConsolidatorRule):
     This post processor make sure that medication or disease won't be wrongly recognized as PHI entity
     """
 
+    MEDICAL_PHRASE_WINDOW_SIZE = 15
+    HEALTHCATRE_PROFESSIONAL_WINDOW_SIZE = 5
+
     def __init__(self):
         """
         Initializes MedicalPostConsolidator
         """
-        super().__init__(supported_entity_types=["DISEASE", "MEDICATION"],
+        super().__init__(supported_entity_types=["DISEASE", "MEDICATION", "MEDICAL_TEST"],
                          lower_preference_entity_types=["PERS", "PER", "ORG", "FAC"],
                          prefer_other_types=False)
 
-        self.medical_prepositions = set(DISEASE_PREPOSITIONS + MEDICATION_PREPOSITIONS)
+        self.medical_prepositions = set(DISEASE_PREPOSITIONS + MEDICATION_PREPOSITIONS + MEDICAL_TEST_PREPOSITIONS)
 
     def __call__(self, consolidated_entities: List[RecognizerResult], custom_entities: List[RecognizerResult],
                  doc: Doc) -> List[RecognizerResult]:
@@ -37,26 +40,40 @@ class MedicalPostConsolidator(PostConsolidatorRule):
         :param doc: document which stores the recognized entities
         """
 
+        post_consolidated_entities = consolidated_entities
         if len(custom_entities) > 0:
-            post_consolidated_entities = []
-            offset_to_medical_entity = MedicalPostConsolidator.map_offset_to_entity(custom_entities)
-            for entity in consolidated_entities:
-                if entity.entity_type in self.lower_preference_entity_types:
-                    overlapping_medical_entities = MedicalPostConsolidator.get_entities_in_span(offset_to_medical_entity,
-                                                                                                entity.start, entity.end)
-                    if len(overlapping_medical_entities) == 0 or not any(
-                            self.is_full_overlap(entity, medical_entity, doc) for medical_entity in
-                            overlapping_medical_entities):
-                        post_consolidated_entities.append(entity)
-                else:
-                    post_consolidated_entities.append(entity)
-        else:
-            post_consolidated_entities = consolidated_entities
+            post_consolidated_entities = self.remove_entity_overlap_with_medical(custom_entities,
+                                                                                 post_consolidated_entities, doc)
 
         post_consolidated_entities = self.infer_by_context(doc, post_consolidated_entities)
         post_consolidated_entities = self.remove_person_not_in_beginning(doc, post_consolidated_entities)
 
         return post_consolidated_entities
+
+    def remove_entity_overlap_with_medical(self, medical_entities: List[RecognizerResult],
+                                           consolidated_entities: List[RecognizerResult], doc: Doc) \
+            -> List[RecognizerResult]:
+        """
+        Removes ORG and NAME entities that overlap with medical entities
+
+        :param medical_entities: recognized medical entities
+        :param consolidated_entities: entities after consolidation
+        :param doc: Doc object
+        :return: updated list of consolidated entities
+        """
+        no_overlap_entities = []
+        offset_to_medical_entity = MedicalPostConsolidator.map_offset_to_entity(medical_entities)
+        for entity in consolidated_entities:
+            if entity.entity_type in self.lower_preference_entity_types:
+                overlapping_medical_entities = MedicalPostConsolidator.get_entities_in_span(offset_to_medical_entity,
+                                                                                            entity.start, entity.end)
+                if len(overlapping_medical_entities) == 0 or not any(
+                        self.is_full_overlap(entity, medical_entity, doc) for medical_entity in
+                        overlapping_medical_entities):
+                    no_overlap_entities.append(entity)
+            else:
+                no_overlap_entities.append(entity)
+        return no_overlap_entities
 
     def is_full_overlap(self, entity: RecognizerResult, medical_entity: RecognizerResult, doc: Doc) -> bool:
         """
@@ -108,7 +125,15 @@ class MedicalPostConsolidator(PostConsolidatorRule):
                 entities.append(offset_to_entity[i])
         return set(entities)
 
-    def infer_by_context(self, doc: Doc, consolidated_entities: List[RecognizerResult]):
+    def infer_by_context(self, doc: Doc, consolidated_entities: List[RecognizerResult]) -> List[RecognizerResult]:
+        """
+        Removes ORG and NAME entities in case that they follow at least one medical context phrase in specific window
+        size
+
+        :param doc: Doc object
+        :param consolidated_entities: recognized entities
+        :return: updated list of consolidated entities
+        """
         medical_rec = TermsRecognizer(CATEGORY_TO_CONTEXT_PHRASES["MEDICAL"])
         offsets = medical_rec(doc.text, list(self.medical_prepositions))
         end_offsets = list(map(lambda offset: offset[0] + offset[1] - 1, offsets))
@@ -116,13 +141,24 @@ class MedicalPostConsolidator(PostConsolidatorRule):
         for entity in consolidated_entities:
             if entity.entity_type in self.lower_preference_entity_types:
                 preceding = list(filter(lambda offset: offset < entity.start, end_offsets))
-                if not any(entity.start - end_offset <= 15 for end_offset in preceding):
+                if not any(
+                        entity.start - end_offset <= MedicalPostConsolidator.MEDICAL_PHRASE_WINDOW_SIZE for end_offset
+                        in preceding):
                     res.append(entity)
             else:
                 res.append(entity)
         return res
 
-    def remove_person_not_in_beginning(self, doc: Doc, consolidated_entities: List[RecognizerResult]):
+    def remove_person_not_in_beginning(self, doc: Doc, consolidated_entities: List[RecognizerResult]) -> List[
+        RecognizerResult]:
+        """
+        Removes NAME entities in case that they don't appear in the first 20% of the document and there isn't at least
+        one healthcare professional phrase that precedes them (in specific window size)
+
+        :param doc: Doc object
+        :param consolidated_entities: recognized entities
+        :return: updated list of consolidated entities
+        """
         healthcare_professional = ["ד\"ר", "דר", "דוקטור", "פרופסור", "פרופ\'", "פרופ", "רופא"]
         healthcare_professional_rec = TermsRecognizer(healthcare_professional)
         offsets = healthcare_professional_rec(doc.text, list(self.medical_prepositions))
@@ -132,7 +168,8 @@ class MedicalPostConsolidator(PostConsolidatorRule):
             if ENTITY_TYPE_TO_CATEGORY[entity.entity_type] == "NAME":
                 if entity.start >= int(0.2 * len(doc.text)):
                     preceding = list(filter(lambda offset: offset < entity.start, end_offsets))
-                    if any(entity.start - end_offset <= 5 for end_offset in preceding):
+                    if any(entity.start - end_offset <= MedicalPostConsolidator.HEALTHCATRE_PROFESSIONAL_WINDOW_SIZE for
+                           end_offset in preceding):
                         res.append(entity)
                 else:
                     res.append(entity)
