b2ea7b95c732f52a80b7965e38d25277befcd3d4
omri374 <omri.mendels@microsoft.com>
2022-03-03 17:39:42 +0200

    updates to imports

--- hebsafeharbor/identifier/phi_identifier.py
+++ hebsafeharbor/identifier/phi_identifier.py
@@ -1,8 +1,17 @@
 from typing import List
 
-from hebsafeharbor.common.city_utils import BELOW_THRESHOLD_CITIES_LIST, ABOVE_THRESHOLD_CITIES_LIST, \
-    ABBREVIATIONS_LIST, AMBIGOUS_BELOW_THRESHOLD_CITIES_LIST, AMBIGOUS_ABOVE_THRESHOLD_CITIES_LIST, \
+from presidio_analyzer import AnalyzerEngine, LocalRecognizer, RecognizerRegistry
+from presidio_analyzer.predefined_recognizers import CreditCardRecognizer, DateRecognizer, EmailRecognizer, \
+    IpRecognizer, PhoneRecognizer, UrlRecognizer
+
+from hebsafeharbor.common.city_utils import (
+    BELOW_THRESHOLD_CITIES_LIST,
+    ABOVE_THRESHOLD_CITIES_LIST,
+    ABBREVIATIONS_LIST,
+    AMBIGOUS_BELOW_THRESHOLD_CITIES_LIST,
+    AMBIGOUS_ABOVE_THRESHOLD_CITIES_LIST,
     AMBIGUOUS_CITIES_CONTEXT
+)
 from hebsafeharbor.common.country_utils import COUNTRY_DICT
 from hebsafeharbor.common.document import Doc
 from hebsafeharbor.common.prepositions import LOCATION_PREPOSITIONS, DISEASE_PREPOSITIONS, MEDICATION_PREPOSITIONS, \
@@ -11,19 +20,18 @@ from hebsafeharbor.identifier import HebSpacyNlpEngine
 from hebsafeharbor.identifier.consolidation.consolidator import NerConsolidator
 from hebsafeharbor.identifier.entity_smoother.entity_smoother_rule_executor import EntitySmootherRuleExecutor
 from hebsafeharbor.identifier.entity_spliters.entity_splitter_rule_executor import EntitySplitterRuleExecutor
-from hebsafeharbor.identifier.signals import SpacyRecognizerWithConfidence
-from hebsafeharbor.identifier.signals.general_id_recognizer import GeneralIdRecognizer
 
-from hebsafeharbor.identifier.signals.heb_date_recognizer import HebDateRecognizer
-from hebsafeharbor.identifier.signals.heb_preposition_date_recognizer import PrepositionDateRecognizer
-from hebsafeharbor.identifier.signals.heb_latin_date_recognizer import HebLatinDateRecognizer
-from hebsafeharbor.identifier.signals.hebrew_city_recognizer import AmbiguousHebrewCityRecognizer
-from hebsafeharbor.identifier.signals.israeli_id_recognizer import IsraeliIdNumberRecognizer
-from presidio_analyzer import AnalyzerEngine, LocalRecognizer, RecognizerRegistry
-from presidio_analyzer.predefined_recognizers import CreditCardRecognizer, DateRecognizer, EmailRecognizer, \
-    IpRecognizer, PhoneRecognizer, SpacyRecognizer, UrlRecognizer
+from hebsafeharbor.identifier.signals import (
+    SpacyRecognizerWithConfidence,
+    GeneralIdRecognizer,
+    HebDateRecognizer,
+    PrepositionDateRecognizer,
+    HebLatinDateRecognizer,
+    AmbiguousHebrewCityRecognizer,
+    IsraeliIdNumberRecognizer,
+    LexiconBasedRecognizer
+)
 
-from hebsafeharbor.identifier.signals.lexicon_based_recognizer import LexiconBasedRecognizer
 from hebsafeharbor.lexicons.disease import DISEASES
 from hebsafeharbor.lexicons.medical_device import MEDICAL_DEVICE
 from hebsafeharbor.lexicons.medical_tests import MEDICAL_TESTS
@@ -148,7 +156,7 @@ class PhiIdentifier:
         #                                                  context=AMBIGUOUS_CITIES_CONTEXT,
         #                                                  ),
         #                    )
-        
+
         # init disease recognizer
         ner_signals.append(
             LexiconBasedRecognizer("DiseaseRecognizer", "DISEASE", DISEASES, allowed_prepositions=DISEASE_PREPOSITIONS))
--- hebsafeharbor/identifier/signals/__init__.py
+++ hebsafeharbor/identifier/signals/__init__.py
@@ -1,5 +1,19 @@
 from .general_id_recognizer import GeneralIdRecognizer
 from .israeli_id_recognizer import IsraeliIdNumberRecognizer
 from .spacy_recognizer_with_confidence import SpacyRecognizerWithConfidence
+from .hebrew_city_recognizer import AmbiguousHebrewCityRecognizer
+from .heb_preposition_date_recognizer import PrepositionDateRecognizer
+from .heb_date_recognizer import HebDateRecognizer
+from .lexicon_based_recognizer import LexiconBasedRecognizer
+from .heb_latin_date_recognizer import HebLatinDateRecognizer
 
-__all__ = ["GeneralIdRecognizer", "IsraeliIdNumberRecognizer", "SpacyRecognizerWithConfidence"]
+__all__ = [
+    "GeneralIdRecognizer",
+    "IsraeliIdNumberRecognizer",
+    "SpacyRecognizerWithConfidence",
+    "AmbiguousHebrewCityRecognizer",
+    "PrepositionDateRecognizer",
+    "HebDateRecognizer",
+    "HebLatinDateRecognizer",
+    "LexiconBasedRecognizer",
+]
